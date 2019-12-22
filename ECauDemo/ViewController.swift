9ea1da49a3befa5e63191e5b019f5652f5f10e81
Endi Caushi <mrendi29@gmail.com>
2019-12-22 01:42:50 -0500

    Required and Optional features implemented.

--- ECauDemo/ViewController.swift
+++ ECauDemo/ViewController.swift
@@ -12,15 +12,36 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var txtWorld: UILabel!
+    var backgroundColor: UIColor!
+    
+    @IBOutlet weak var txtField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        backgroundColor = view.backgroundColor
+        
     }
 
     
     @IBAction func btnTap(_ sender: Any) {
         txtWorld.textColor = UIColor.orange
+    }
+    
+    @IBAction func btnViewTap(_ sender: Any) {
+        view.backgroundColor = UIColor.red
+        
+    }
+    @IBAction func btnText(_ sender: Any) {
+        
+        txtWorld.text = txtField.text
+        txtField.text = ""
+        
+    }
+    @IBAction func onViewReset(_ sender: Any) {
+        txtWorld.text = "Hello From Endi"
+        view.backgroundColor = backgroundColor
+        
     }
     
 }
